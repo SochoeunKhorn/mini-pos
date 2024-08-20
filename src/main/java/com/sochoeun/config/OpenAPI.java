e8c367155f67d7e529bfb4c5a98522ec2dc1584a
Sochoeun <sochoeun1202@gmail.com>
2024-08-20 14:06:10 +0700

    add Open API Docs

---
+++ src/main/java/com/sochoeun/config/OpenAPI.java
@@ -0,0 +1,7 @@
+package com.sochoeun.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPI {
+}
--- src/main/java/com/sochoeun/controller/BrandController.java
+++ src/main/java/com/sochoeun/controller/BrandController.java
@@ -5,6 +5,7 @@ import com.sochoeun.dto.PageResponse;
 import com.sochoeun.mapper.BrandMapper;
 import com.sochoeun.model.Brand;
 import com.sochoeun.service.BrandService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/brands")
 @RequiredArgsConstructor
+@Tag(name="Brand")
 public class BrandController {
     private static final Logger log = LoggerFactory.getLogger(BrandController.class);
     private final BrandService brandService;
